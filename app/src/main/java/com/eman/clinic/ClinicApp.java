95b575c89d2bd63510f4060e7509de884ab7eba6
emee96-ai <emee199611@gmail.com>
2026-09-20 10:16:06 +0200

    Start sync scheduler with app process

--- app/src/main/java/com/eman/clinic/ClinicApp.java
+++ app/src/main/java/com/eman/clinic/ClinicApp.java
@@ -6,5 +6,6 @@ public class ClinicApp extends Application {
     @Override public void onCreate() {
         super.onCreate();
         SyncBootstrap.install(this);
+        SyncCoordinator.start(this);
     }
 }
