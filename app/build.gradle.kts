d9d068326a26ef8adb90a4b822a1d40f27bf2410
emee96-ai <emee199611@gmail.com>
2026-09-22 00:45:37 +0200

    Bump Clinic to v1.11.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.eman.clinic"
         minSdk = 24
         targetSdk = 35
-        versionCode = 21
-        versionName = "1.10.0"
+        versionCode = 22
+        versionName = "1.11.0"
     }
 
     compileOptions {
